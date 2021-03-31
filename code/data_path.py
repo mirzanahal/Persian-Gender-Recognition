c087ec0d81b367597c3a2936db8c55c28976cc10
Nahal <mirzanahal@gmail.com>
2021-03-31 21:24:40 +0430

    fasttext added

--- code/data_path.py
+++ code/data_path.py
@@ -5,6 +5,8 @@ POSITIVE_WORDS_PATH = '../data/positive_words.npy'
 NEGATIVE_WORDS_PATH = '../data/negative_words.npy'
 
 POSTAGGER_MODEL_PATH = '../model/postagger.model'
+FASTTEXT_PERSIAN_MODEL_PATH = '../model/cc.fa.300.bin'
+
 
 GENDER_RECOGNITION_MODEL_PATH = '../model/gender_recognition_model.pkl'
 
