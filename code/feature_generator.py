3b99305fb6d35bbc26f465db4556e31797572466
Nahal <mirzanahal@gmail.com>
2021-03-31 22:59:43 +0430

    minor changes

--- code/feature_generator.py
+++ code/feature_generator.py
@@ -7,7 +7,7 @@ import utils
 
 
 
-def generate_features():
+def generate_features(path):
     if (not isfile(data_path.POSITIVE_WORDS_PATH)) or (not isfile(data_path.NEGATIVE_WORDS_PATH)):
         utils.import_positive_negative_words_from(
             path = data_path.EMOTION_LEXICON_WORDS_PATH,
@@ -16,34 +16,34 @@ def generate_features():
         )
 
     #embedding_gensim
-    if (not isfile(join(data_path.DATA_PATH, 'embedded_texts_gensim.csv'))):
-        system('python generate_embedding_gensim.py --verbose')
-    embedding_features = pd.read_csv(join(data_path.DATA_PATH, 'embedded_texts_gensim.csv'), index_col=0)
+    if (not isfile(join(data_path.DATA_PATH, path, 'embedded_texts_gensim.csv'))):
+        system('python generate_embedding_gensim.py --verbose --output {}'.format(path))
+    embedding_features = pd.read_csv(join(data_path.DATA_PATH, path, 'embedded_texts_gensim.csv'), index_col=0)
 
     #psychological_features               
-    if (not isfile(join(data_path.DATA_PATH, 'psychological_features.csv'))):
-        system('python generate_psychological_features.py --verbose')
-    psychological_features = pd.read_csv(join(data_path.DATA_PATH, 'psychological_features.csv'), index_col=0)               
+    if (not isfile(join(data_path.DATA_PATH, path, 'psychological_features.csv'))):
+        system('python generate_psychological_features.py --verbose --output {}'.format(path))
+    psychological_features = pd.read_csv(join(data_path.DATA_PATH, path, 'psychological_features.csv'), index_col=0)               
 
     #structural_features
-    if (not isfile(join(data_path.DATA_PATH, 'structural_features.csv'))):
-        system('python generate_structural_features.py --verbose')
-    structural_features = pd.read_csv(join(data_path.DATA_PATH, 'structural_features.csv'), index_col=0)               
+    if (not isfile(join(data_path.DATA_PATH, path, 'structural_features.csv'))):
+        system('python generate_structural_features.py --verbose --output {}'.format(path))
+    structural_features = pd.read_csv(join(data_path.DATA_PATH, path, 'structural_features.csv'), index_col=0)               
                 
     #syntactics_features
-    if (not isfile(join(data_path.DATA_PATH, 'syntactics_features.csv'))):
-        system('python generate_syntactics_features.py --verbose')
-    syntactics_features = pd.read_csv(join(data_path.DATA_PATH, 'syntactics_features.csv'), index_col=0)                   
+    if (not isfile(join(data_path.DATA_PATH, path, 'syntactics_features.csv'))):
+        system('python generate_syntactics_features.py --verbose --output {}'.format(path))
+    syntactics_features = pd.read_csv(join(data_path.DATA_PATH, path, 'syntactics_features.csv'), index_col=0)                   
                 
     #text_dependent_features
-    if (not isfile(join(data_path.DATA_PATH, 'text_dependent_features.csv'))):
-        system('python generate_text_dependent_features.py --verbose')
-    text_dependent_features = pd.read_csv(join(data_path.DATA_PATH, 'text_dependent_features.csv'), index_col=0)               
+    if (not isfile(join(data_path.DATA_PATH, path, 'text_dependent_features.csv'))):
+        system('python generate_text_dependent_features.py --verbose --output {}'.format(path))
+    text_dependent_features = pd.read_csv(join(data_path.DATA_PATH, path, 'text_dependent_features.csv'), index_col=0)               
                 
     #word_dependent_features
-    if (not isfile(join(data_path.DATA_PATH, 'word_dependent_features.csv'))):
-        system('python generate_word_dependent_features.py --verbose')
-    word_dependent_features = pd.read_csv(join(data_path.DATA_PATH, 'word_dependent_features.csv'), index_col=0)                   
+    if (not isfile(join(data_path.DATA_PATH, path, 'word_dependent_features.csv'))):
+        system('python generate_word_dependent_features.py --verbose --output {}'.format(path))
+    word_dependent_features = pd.read_csv(join(data_path.DATA_PATH, path, 'word_dependent_features.csv'), index_col=0)                   
                 
     features_list = [embedding_features, psychological_features, structural_features, syntactics_features, text_dependent_features, word_dependent_features]
 
